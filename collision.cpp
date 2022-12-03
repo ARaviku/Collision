14c6fa8774ea0fc2393e5346c6f8c766c46d7d7a
ARaviku <100729318+ARaviku@users.noreply.github.com>
2022-12-03 15:09:05 -0500

    Update collision.cpp

--- collision.cpp
+++ collision.cpp
@@ -8,15 +8,12 @@
 
 int main(int argc, char* argv[])
 {
-    // when submitting to autograder, make sure you use exactly the 'main' function we gave you!
     Disc robot{0, 0, 1}; // dummy values initially
     Rectangle rect1{0, 0, 1, 1};
     Rectangle rect2{2, 1, 3, 2};
     Disc disc1{2, 1, 0.5};
 
     std::vector<Obstacle const *> world{&rect1, &rect2, &disc1};
-
-    // by defult, use the file robot_positions.txt, but if you supply a command line argument, use that instead
     std::string input_filename{"robot_positions.txt"};
     if (argc >= 2) {
         input_filename = argv[1];
